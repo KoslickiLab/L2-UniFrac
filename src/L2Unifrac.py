9dcbeacaf708fc201ba1821dbe8ff7eaea258cbf
Andrew Millward <andrew.j.millward@gmail.com>
2021-03-17 13:04:09 -0400

    Updates

--- src/L2Unifrac.py
+++ src/L2Unifrac.py
@@ -1,4 +1,46 @@
 import numpy as np
+import dendropy
+import sys
+import warnings
+
+epsilon = sys.float_info.epsilon
+
+def parse_tree_file(tree_str_file, suppress_internal_node_taxa=True, suppress_leaf_node_taxa=False):
+    '''
+    (Tint,lint,nodes_in_order) = parse_tree(tree_str_file)
+    This function will parse a newick tree file (in the file given by tree_str_file) and return the dictionary of ancestors Tint.
+    Tint indexes the nodes by integers, Tint[i] = j means j is the ancestor of i.
+    lint is a dictionary returning branch lengths: lint[i,j] = w(i,j) the weight of the edge connecting i and j.
+    nodes_in_order is a list of the nodes in the input tree_str such that T[i]=j means nodes_in_order[j] is an ancestor
+    of nodes_in_order[i]. Nodes are labeled from the leaves up.
+    '''
+    dtree = dendropy.Tree.get(path=tree_str_file, schema="newick",
+                            suppress_internal_node_taxa=suppress_internal_node_taxa,
+                            store_tree_weights=True,
+                            suppress_leaf_node_taxa = suppress_leaf_node_taxa)
+    #Name all the internal nodes
+    nodes = dtree.nodes()
+    i=0
+    for node in nodes:
+        if node.taxon == None:
+            node.taxon = dendropy.datamodel.taxonmodel.Taxon(label="temp"+str(i))
+            i = i+1
+    full_nodes_in_order = [item for item in dtree.levelorder_node_iter()]  # i in path from root to j only if i>j
+    full_nodes_in_order.reverse()
+    nodes_in_order = [item.taxon.label for item in full_nodes_in_order]  # i in path from root to j only if i>j
+    Tint = dict()
+    lint = dict()
+    nodes_to_index = dict(zip(nodes_in_order, range(len(nodes_in_order))))
+    for i in range(len(nodes_in_order)):
+        node = full_nodes_in_order[i]
+        parent = node.parent_node
+        if parent != None:
+            Tint[i] = nodes_to_index[parent.taxon.label]
+            if isinstance(node.edge.length, float):
+                lint[nodes_to_index[node.taxon.label], nodes_to_index[parent.taxon.label]] = node.edge.length
+            else:
+                lint[nodes_to_index[node.taxon.label], nodes_to_index[parent.taxon.label]] = 0.0
+    return (Tint,lint,nodes_in_order)
 
 def L2Unifrac_weighted_plain(ancestors, edge_lengths, nodes_in_order, P, Q):
 	'''
@@ -55,3 +97,54 @@ def mean_of_vectors(L):
 	return np.mean(L, axis=0)
 
 # COMPARE PUSHUP AND INVERSE PUSHUP WITH TESTS TO SEE THAT THEY CHANGE OR NOT
+
+def create_env(sample_file):
+    '''
+    :param sample_file: a file containding ids and samples
+    :return: an env_dict in the form of { id: {sample:count} }
+    '''
+    env_dict = dict()
+    with open(sample_file) as fp:
+        line = fp.readline()
+        while line:
+            list = line.split()
+            key = list.pop(0) #get key
+            env_dict[key] = dict()
+            for str in list:
+                sample = str.split('_')[0]
+                if sample in env_dict[key]:
+                    env_dict[key][sample] += 1
+                else:
+                    env_dict[key][sample] = 1
+            line = fp.readline()
+    fp.close()
+    return env_dict
+
+def parse_envs(envs, nodes_in_order):
+    '''
+    (envs_prob_dict, samples) = parse_envs(envs, nodes_in_order)
+    This function takes an environment envs and the list of nodes nodes_in_order and will return a dictionary envs_prob_dict
+    with keys given by samples. envs_prob_dict[samples[i]] is a probability vector on the basis nodes_in_order denoting for sample i.
+    '''
+    nodes_in_order_dict = dict(zip(nodes_in_order,range(len(nodes_in_order))))
+    for node in envs.keys():
+        if node not in nodes_in_order_dict:
+            print("Warning: environments contain taxa " + node + " not present in given taxonomic tree. Ignoring")
+    envs_prob_dict = dict()
+    for i in range(len(nodes_in_order)):
+        node = nodes_in_order[i]
+        if node in envs:
+            samples = envs[node].keys()
+            for sample in samples:
+                if sample not in envs_prob_dict:
+                    envs_prob_dict[sample] = np.zeros(len(nodes_in_order))
+                    envs_prob_dict[sample][i] = envs[node][sample]
+                else:
+                    envs_prob_dict[sample][i] = envs[node][sample]
+    #Normalize samples
+    samples = envs_prob_dict.keys()
+    for sample in samples:
+        if envs_prob_dict[sample].sum() == 0:
+            warnings.warn("Warning: the sample %s has non-zero counts, do not use for Unifrac calculations" % sample)
+        envs_prob_dict[sample] = envs_prob_dict[sample]/envs_prob_dict[sample].sum()
+    return (envs_prob_dict, samples)
--- tests/testPresenceOfNegatives.py
+++ tests/testPresenceOfNegatives.py
@@ -1,30 +1,30 @@
 import sys
-sys.path.append('../MedianUnifrac')
-sys.path.append('../MedianUnifrac/src')
+sys.path.append('../L2Unifrac')
+sys.path.append('../L2Unifrac/src')
 sys.path.append('../src')
-import MedianUnifrac as MedU
+import L2Unifrac as L2U
 import numpy as np
 
 try:
-		(Tint, lint, nodes_in_order) = MedU.parse_tree_file('../data/97_otus_unannotated.tree')
-		env_dict = MedU.create_env('../data/289_seqs_otus.txt')
+		(Tint, lint, nodes_in_order) = L2U.parse_tree_file('../data/97_otus_unannotated.tree')
+		env_dict = L2U.create_env('../data/289_seqs_otus.txt')
 except FileNotFoundError:
-		(Tint, lint, nodes_in_order) = MedU.parse_tree_file('../data/97_otus_unannotated.tree')
-		env_dict = MedU.create_env('../data/289_seqs_otus.txt')
-(env_prob_dict, samples) = MedU.parse_envs(env_dict, nodes_in_order)
+		(Tint, lint, nodes_in_order) = L2U.parse_tree_file('../data/97_otus_unannotated.tree')
+		env_dict = L2U.create_env('../data/289_seqs_otus.txt')
+(env_prob_dict, samples) = L2U.parse_envs(env_dict, nodes_in_order)
 
 def test_median(Ps, Tint, lint, nodes_in_order): #take a list of vectors, push up, take median, then push down
 		Ps_pushed = []
 		for P in Ps:
-				P_pushed = MedU.push_up(P, Tint, lint, nodes_in_order)
+				P_pushed = L2U.push_up(P, Tint, lint, nodes_in_order)
 				Ps_pushed.append(P_pushed)
-		median = MedU.mean_of_vectors(Ps_pushed)
-		median_inverse = MedU.inverse_push_up(median, Tint, lint, nodes_in_order)
-		print np.sum(median_inverse < 0)
+		median = L2U.mean_of_vectors(Ps_pushed)
+		median_inverse = L2U.inverse_push_up(median, Tint, lint, nodes_in_order)
+		print (np.sum(median_inverse < 0))
 		return np.any(median_inverse < 0)
 
 is_negative = []
-num_its = 100
+num_its = 1
 for num_vectors in range(3, len(samples), 5):
 		print (num_vectors)
 		for i in range(num_its):
@@ -34,5 +34,5 @@ for num_vectors in range(3, len(samples), 5):
 						Ps.append(env_prob_dict[sample])
 				is_negative.append(test_median(Ps, Tint, lint, nodes_in_order))
 
-print np.sum(is_negative)
-print np.sum(is_negative)/(len(is_negative))
+print (np.sum(is_negative))
+print (np.sum(is_negative)/(len(is_negative)))
