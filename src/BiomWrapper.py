04ef555226bd7e6936a789d5cd828ec58f55be3a
andrew-j-millward <andrew.j.millward@gmail.com>
2021-04-10 18:43:26 -0400

    Connected git to local repo (finally)

--- src/BiomWrapper.py
+++ src/BiomWrapper.py
@@ -1,10 +1,20 @@
 import biom
+import numpy as np
 
 def extract_biom(address):
 	Biom = biom.load_table(address)
 	print(Biom.ids()) 
 	print(Biom.ids(axis='observation')) 
 	print(Biom.nnz)
+	#transform_f = lambda v,i,m: np.where(v % 3 == 0, v, 0)
+	#mult_of_three = tform = Biom.transform(transform_f, inplace=False)
+	#print(mult_of_three) 
+	#phylum_idx = 1
+	#collapse_f = lambda id_, md: '; '.join(md['taxonomy'][:phylum_idx + 1])
+	#collapsed = mult_of_three.collapse(collapse_f, axis='observation')
+	#print(collapsed) 
+	#pa = collapsed.pa()
+	#print(pa)
 	#print(Biom)
 
 if __name__ == '__main__':
