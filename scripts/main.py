b9e91b87eaf4289cbd9b422f6c4d1dc101ad209c
andrew-j-millward <andrew.j.millward@gmail.com>
2021-05-25 22:04:35 -0400

    Fixes #3

--- scripts/main.py
+++ scripts/main.py
@@ -9,7 +9,7 @@ import numpy as np
 
 if __name__ == "__main__":
 	nodes_samples = BW.extract_biom('../data/47422_otu_table.biom')
-	T1, l1, nodes1 = L2U.parse_tree_file('../data/trees/gg_97_otus_4feb2011.tre')
+	T1, l1, nodes1 = L2U.parse_tree_file('../data/trees/gg_13_5_otus_99_annotated.tree')
 	(nodes_weighted, samples_temp) = L2U.parse_envs(nodes_samples, nodes1)
 	EMDUnifrac = L2U.L2Unifrac_weighted_plain(T1, l1, nodes_samples, nodes_weighted['1928.SRS058420.SRX020544.SRR045915'], nodes_weighted['1928.SRS011433.SRX020669.SRR045315'])
 	print(EMDUnifrac)
