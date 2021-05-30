b015de6937555bb201a29f9009cf24b43903566b
andrew-j-millward <andrew.j.millward@gmail.com>
2021-05-30 13:55:12 -0400

    Added CSV generator

---
+++ scripts/write_to_csv.py
@@ -0,0 +1,35 @@
+################################################################################
+##
+##  File           : write_to_csv.py
+##  Description    : Writes UniFrac data to CSV file periodically. 
+##
+##   Author        : *** Andrew Millward ***
+##   Last Modified : *** 05/30/2021 ***
+##
+
+## Import Files
+import csv
+
+################################################################################
+##
+## Function     : write_spreadsheet
+## Description  : Takes input data and exports it to a new spreadsheet.
+##
+## Inputs       : elem_list - List of elements to write from query.
+## Outputs      : 0 if successful, -1 if failure.
+
+def write_spreadsheet(name, dist_list):
+
+    try:
+        with open(name, 'a', newline='') as csvfile:
+            file_write = csv.writer(csvfile)
+            file_write.writerow(dist_list)
+            csvfile.close()
+
+        return ( 0 ) # Success
+
+    except:
+        return ( -1 ) # Failure
+
+if __name__ == "__main__":
+    write_spreadsheet('distance_matrix.csv', [0.3, 0.2, 0.4])
