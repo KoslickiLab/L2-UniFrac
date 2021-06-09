cc9b83dff6ddb530188089126fa2ce4dab4f8853
andrew-j-millward <andrew.j.millward@gmail.com>
2021-06-09 01:20:36 -0400

    Maximized core usage, output adjustments

--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('../L2Unifrac')
 sys.path.append('../L2Unifrac/src')
 sys.path.append('../src')
-import L2Unifrac as L2U 
+import L2Unifrac as L2U
 import BiomWrapper as BW
 import write_to_csv as CSV
 import multiprocessing
@@ -24,39 +24,39 @@ Distance_Matrix = []
 PCoA_Samples = PCoA_Samples[:64]
 
 def unifrac_work_wrapper(args):
-		#print(args)
-		return unifrac_worker(*args)
+	#print(args)
+	return unifrac_worker(*args)
 
 def unifrac_worker(samp1num, samp2num):
-		#print(f"Samples: {samp1num}, {samp2num}")
-		#print(f"PCoA_Samples: {PCoA_Samples}")
-		L2UniFrac = L2U.L2Unifrac_weighted_plain(T1, l1, nodes_in_order, nodes_weighted[PCoA_Samples[samp1num]], nodes_weighted[PCoA_Samples[samp2num]])
-		formatted_L2 = "{:.16f}".format(L2UniFrac)
-		return L2UniFrac, f"\tInner loop: {str(samp2num).zfill(4)} | L2-UniFrac: {formatted_L2} | Sample 1: {PCoA_Samples[samp1num]} | Sample 2: {PCoA_Samples[samp2num]}"
+	#print(f"Samples: {samp1num}, {samp2num}")
+	#print(f"PCoA_Samples: {PCoA_Samples}")
+	L2UniFrac = L2U.L2Unifrac_weighted_plain(T1, l1, nodes_in_order, nodes_weighted[PCoA_Samples[samp1num]], nodes_weighted[PCoA_Samples[samp2num]])
+	formatted_L2 = "{:.16f}".format(L2UniFrac)
+	return L2UniFrac, f"\tInner loop: {str(samp2num).zfill(4)} | L2-UniFrac: {formatted_L2} | Sample 1: {PCoA_Samples[samp1num]} | Sample 2: {PCoA_Samples[samp2num]}"
 
 if __name__ == "__main__":
 
-		local_vars = list(locals().items())
-		for var, obj in local_vars:
-				print(f"{var.ljust(17)}: {sys.getsizeof(obj)}")
+	local_vars = list(locals().items())
+	for var, obj in local_vars:
+		print(f"{var.ljust(17)}: {sys.getsizeof(obj)}")
 
-		# Multi Core Method
-		#for i in range(len(PCoA_Samples)):
+	# Multi Core Method
+	#for i in range(len(PCoA_Samples)):
 
-		#        print(f"Iteration row: {i}")
+	#        print(f"Iteration row: {i}")
 
-		row = [[(i, j) for j in range(len(PCoA_Samples))] for i in range(len(PCoA_Samples))]
+	row = [(i, j) for j in range(len(PCoA_Samples)) for i in range(len(PCoA_Samples))]
 
-		with multiprocessing.Pool(processes=128) as pool:
-			result = pool.map(unifrac_work_wrapper, row)
+	with multiprocessing.Pool(processes=cores-1) as pool:
+		result = pool.map(unifrac_work_wrapper, row)
 
-		for i in range(len(PCoA_Samples)):
-			dist_list = []
-			for j in range(len(PCoA_Samples)):
-					dist_list.append(result[i][j])
-					print(result[i*len(PCoA_Samples)+j])
+	for i in range(len(PCoA_Samples)):
+		dist_list = []
+		for j in range(len(PCoA_Samples)):
+			dist_list.append(result[i*len(PCoA_Samples)+j][0])
+			print(result[i*len(PCoA_Samples)+j][1])
 
-			CSV.write('L2-UniFrac-Out.csv', dist_list)
+		CSV.write('L2-UniFrac-Out.csv', dist_list)
 
 	# Single Core Method
 	#for i in range(len(PCoA_Samples)):
