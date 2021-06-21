8b74a4ace9298216fae6866e3612435665011d07
andrew-j-millward <andrew.j.millward@gmail.com>
2021-06-21 19:01:47 -0400

    Cleared unnecessary files, added toggle for group-based unifrac

--- scripts/pairwise_unifrac.py
+++ scripts/pairwise_unifrac.py
@@ -9,13 +9,11 @@ import multiprocessing
 
 cores = multiprocessing.cpu_count()
 
-nodes_samples = BW.extract_biom('../data/47422_otu_table.biom')
-T1, l1, nodes_in_order = L2U.parse_tree_file('../data/trees/gg_13_5_otus_99_annotated.tree')
-(nodes_weighted, samples_temp) = L2U.parse_envs(nodes_samples, nodes_in_order)
+#nodes_samples = BW.extract_biom('../data/47422_otu_table.biom')
+#T1, l1, nodes_in_order = L2U.parse_tree_file('../data/trees/gg_13_5_otus_99_annotated.tree')
+#(nodes_weighted, samples_temp) = L2U.parse_envs(nodes_samples, nodes_in_order)
 
-PCoA_Samples = BW.extract_samples('../data/47422_otu_table.biom')
-
-Distance_Matrix = []
+#PCoA_Samples = BW.extract_samples('../data/47422_otu_table.biom')
 
 def unifrac_work_wrapper(args):
 	return unifrac_worker(*args)
@@ -25,17 +23,7 @@ def unifrac_worker(samp1num, samp2num):
 	formatted_L2 = "{:.16f}".format(L2UniFrac)
 	return L2UniFrac, f"\tInner loop: {str(samp2num).zfill(4)} | L2-UniFrac: {formatted_L2} | Sample 1: {PCoA_Samples[samp1num]} | Sample 2: {PCoA_Samples[samp2num]}"
 
-if __name__ == "__main__":
-
-	args = sys.argv
-	if len(args) > 1:
-		if args[1] == "1":
-			debug = 1
-		else:
-			debug = 0
-	else:
-		debug = 0
-
+def Total_Pairwise(debug):
 	if debug == 1:
 		print(f"Running Debugging Multiprocess on {cores-1} Cores...")
 
@@ -60,3 +48,38 @@ if __name__ == "__main__":
 				print(result[i*len(PCoA_Samples)+j][1])
 
 		CSV.write('L2-UniFrac-Out.csv', dist_list)
+
+def Group_Pairwise(debug):
+	pass
+
+if __name__ == "__main__":
+
+	args = sys.argv
+	if len(args) == 3:
+		if args[1] == "-v" or args[2] == "-v":
+			debug = 1
+		else:
+			debug = 0
+		if args[2] == "0" or args[1] == "0":
+			toggle = 0
+		else:
+			toggle = 1
+	elif len(args) == 2:
+		if args[1] == "-v":
+			debug = 1
+		else:
+			debug = 0
+		if args[1] == "0":
+			toggle = 0
+		else:
+			toggle = 1
+	else:
+		debug = 0
+		toggle = 1
+
+	if toggle:
+		Group_Pairwise(debug)
+	else:
+		Total_Pairwise(debug)
+
+	
