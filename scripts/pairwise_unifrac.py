ab6709b67f2d5c61f0c945921b7f290d24395e24
andrew-j-millward <andrew.j.millward@gmail.com>
2021-06-22 17:48:12 -0400

    Limited group scope to fix too many process bug

--- scripts/pairwise_unifrac.py
+++ scripts/pairwise_unifrac.py
@@ -10,11 +10,11 @@ import multiprocessing as mp
 
 cores = mp.cpu_count()
 
-nodes_samples = BW.extract_biom('../data/47422_otu_table.biom')
-T1, l1, nodes_in_order = L2U.parse_tree_file('../data/trees/gg_13_5_otus_99_annotated.tree')
-(nodes_weighted, samples_temp) = L2U.parse_envs(nodes_samples, nodes_in_order)
+#nodes_samples = BW.extract_biom('../data/47422_otu_table.biom')
+#T1, l1, nodes_in_order = L2U.parse_tree_file('../data/trees/gg_13_5_otus_99_annotated.tree')
+#(nodes_weighted, samples_temp) = L2U.parse_envs(nodes_samples, nodes_in_order)
 
-PCoA_Samples = BW.extract_samples('../data/47422_otu_table.biom')
+#PCoA_Samples = BW.extract_samples('../data/47422_otu_table.biom')
 
 def unifrac_work_wrapper(args):
 	return unifrac_worker(*args)
@@ -50,7 +50,8 @@ def Total_Pairwise(debug):
 
 		CSV.write('L2-UniFrac-Out.csv', dist_list)
 
-def Group_Pairwise(debug):
+def Group_Pairwise(debug, group_num):
+	group_num -= 1
 	metadata = meta.extract_metadata('../data/metadata/P_1928_65684500_raw_meta.txt')
 	sample_groups = []
 	groups_temp = list(metadata.values())
@@ -69,60 +70,58 @@ def Group_Pairwise(debug):
 				sample_sites[j].append(PCoA_Samples[i])
 	print(sample_sites)
 
-	for group_num in range(len(sample_sites)):
-		if debug == 1:
-			print(f"Running Debugging Multiprocess on {cores-1} Cores...")
+	if debug == 1:
+		print(f"Running Debugging Multiprocess on {cores-1} Cores...")
 
-			# Testing subset of samples...
-			sample_sites[group_num] = sample_sites[group_num][:64]
-			
-			local_vars = list(locals().items())
-			for var, obj in local_vars:
-				print(f"{var.ljust(17)}: {sys.getsizeof(obj)}")
+		# Testing subset of samples...
+		sample_sites[group_num] = sample_sites[group_num][:64]
+		
+		local_vars = list(locals().items())
+		for var, obj in local_vars:
+			print(f"{var.ljust(17)}: {sys.getsizeof(obj)}")
 
-		# Multi Core Method
-		row = [(i, j) for j in range(len(sample_sites[group_num])) for i in range(len(sample_sites[group_num]))]
+	# Multi Core Method
+	row = [(i, j) for j in range(len(sample_sites[group_num])) for i in range(len(sample_sites[group_num]))]
 
-		with mp.Pool(processes=cores-1) as pool:
-			result = pool.map(unifrac_work_wrapper, row)
+	with mp.Pool(processes=cores-1) as pool:
+		result = pool.map(unifrac_work_wrapper, row)
 
-		for i in range(len(sample_sites[group_num])):
-			dist_list = []
-			for j in range(len(sample_sites[group_num])):
-				dist_list.append(result[i*len(sample_sites[group_num])+j][0])
-				if debug == 1:
-					print(result[i*len(sample_sites[group_num])+j][1])
+	for i in range(len(sample_sites[group_num])):
+		dist_list = []
+		for j in range(len(sample_sites[group_num])):
+			dist_list.append(result[i*len(sample_sites[group_num])+j][0])
+			if debug == 1:
+				print(result[i*len(sample_sites[group_num])+j][1])
 
-			CSV.write('L2-UniFrac-Out-' + groups[group_num] + '.csv', dist_list)
+		CSV.write('L2-UniFrac-Out-' + groups[group_num] + '.csv', dist_list)
 
 if __name__ == "__main__":
 
 	args = sys.argv
-	if len(args) == 3:
-		if args[1] == "-v" or args[2] == "-v":
-			debug = 1
-		else:
-			debug = 0
-		if args[2] == "0" or args[1] == "0":
-			toggle = 0
-		else:
-			toggle = 1
-	elif len(args) == 2:
-		if args[1] == "-v":
-			debug = 1
-		else:
-			debug = 0
-		if args[1] == "0":
-			toggle = 0
-		else:
-			toggle = 1
+	for i in range(len(args)):
+		try:
+			args[i] = int(args[i])
+		except:
+			pass
+	if len(args) > 3:
+		raise Exception("Invalid number of parameters.")
+	
+	if "-h" in args:
+		debug = 1
 	else:
 		debug = 0
-		toggle = 1
 
-	if toggle:
-		Group_Pairwise(debug)
+	if len(args) > 1 and isinstance(args[1], int):
+		group_num = args[1]
+	elif len(args) > 2 and isinstance(args[2], int):
+		group_num = args[2]
 	else:
+		group_num = 0
+
+	if group_num:
+		Group_Pairwise(debug, group_num)
+	else:
+		pass
 		Total_Pairwise(debug)
 
 	
