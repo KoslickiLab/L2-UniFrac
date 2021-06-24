a3874c33cf304c9e303e5d47185ac5df7ae9c00c
andrew-j-millward <andrew.j.millward@gmail.com>
2021-06-23 21:16:47 -0400

    More work on clustering

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -5,6 +5,10 @@ sys.path.append('../src')
 import csv
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
+from sklearn_extra.cluster import KMedoids
+import BiomWrapper as BW
+import metadata_wrapper as meta
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
@@ -24,30 +28,23 @@ if __name__ == "__main__":
 	for i in read:
 		distance_matrix.append(list(map(float, i[0].split(","))))
 
-	#sk_distance_matrix = DistanceMatrix(distance_matrix, BW.extract_samples('../data/47422_otu_table.biom'))
+	cluster = AgglomerativeClustering(n_clusters=5, affinity='precomputed', linkage = 'average').fit(distance_matrix)  
 
-	agg = AgglomerativeClustering(n_clusters=5, affinity='precomputed', linkage = 'complete')
-	agg2 = agg.fit_predict(distance_matrix)  # Returns class labels.
+	print(cluster)
 
-	print(agg)
-	print(agg2)
+	print(np.where(cluster == 0)[0])
+	print(np.where(cluster == 1)[0])
+	print(np.where(cluster == 2)[0])
+	print(np.where(cluster == 3)[0])
+	print(np.where(cluster == 4)[0])
 
-	print(np.where(agg2 == 0))
-	print(np.where(agg2 == 1))
-	print(np.where(agg2 == 2))
-	print(np.where(agg2 == 3))
-	print(np.where(agg2 == 4))
+	print(np.where(cluster == 0)[0].size)
+	print(np.where(cluster == 1)[0].size)
+	print(np.where(cluster == 2)[0].size)
+	print(np.where(cluster == 3)[0].size)
+	print(np.where(cluster == 4)[0].size)
 
-	#metadata = meta.extract_metadata('../data/metadata/P_1928_65684500_raw_meta.txt')
+	#np.set_printoptions(threshold=sys.maxsize)
 
-	#pd_metadata = pd.DataFrame.from_dict(metadata, orient='index')
-
-	#result = pcoa(sk_distance_matrix)
-	#print(result)
-
-	#fig = result.plot(df=pd_metadata, column='body_site',
-	#						axis_labels=('PC 1', 'PC 2', 'PC 3'),
-	#						title='Samples colored by body site',
-	#						cmap='Set1', s=50)
-
-	#plt.show()
+	#kmedoids = KMedoids(n_clusters=5, metric='precomputed', method='pam', init='heuristic').fit(distance_matrix)
+	#labels = kmedoids.labels_
