668abb964b6305bcf37282c95b5f277198f3da49
andrew-j-millward <andrew.j.millward@gmail.com>
2021-06-24 13:29:33 -0400

    Sample clustering and baseline classification array

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -16,11 +16,14 @@ if __name__ == "__main__":
 	if len(args) > 3:
 		raise Exception("Invalid number of parameters.")
 	elif len(args) == 1:
-		file = 'L2-UniFrac-Out.csv'
+		file = 'L1-UniFrac-Out.csv'
+		num_clusters = 5
+	elif len(args) == 2:
+		file = args[1]
+		num_clusters = 5
 	else:
 		file = args[1]
-
-	assert(isinstance(file, str))
+		num_clusters = int(args[2])
 
 	f = open(file, 'r')
 	read = csv.reader(f, delimiter=';')
@@ -28,21 +31,25 @@ if __name__ == "__main__":
 	for i in read:
 		distance_matrix.append(list(map(float, i[0].split(","))))
 
-	cluster = AgglomerativeClustering(n_clusters=5, affinity='precomputed', linkage = 'average').fit(distance_matrix)  
+	cluster = AgglomerativeClustering(n_clusters=num_clusters, affinity='precomputed', linkage = 'complete').fit_predict(distance_matrix)  
 
 	print(cluster)
 
-	print(np.where(cluster == 0)[0])
-	print(np.where(cluster == 1)[0])
-	print(np.where(cluster == 2)[0])
-	print(np.where(cluster == 3)[0])
-	print(np.where(cluster == 4)[0])
+	cluster_arr = []
+	for i in range(num_clusters):
+		cluster_arr.append(np.where(cluster == i)[0])
+	print(cluster_arr)
 
-	print(np.where(cluster == 0)[0].size)
-	print(np.where(cluster == 1)[0].size)
-	print(np.where(cluster == 2)[0].size)
-	print(np.where(cluster == 3)[0].size)
-	print(np.where(cluster == 4)[0].size)
+	PCoA_Samples = BW.extract_samples('../data/47422_otu_table.biom')
+	metadata = meta.extract_metadata('../data/metadata/P_1928_65684500_raw_meta.txt')
+	for i in range(len(PCoA_Samples)):
+		PCoA_Samples[i] = metadata[PCoA_Samples[i]]['body_site']
+
+	print(PCoA_Samples)
+
+	#for i in range(len(cluster_arr)):
+		#for j in range(len(cluster_arr[i])):
+
 
 	#np.set_printoptions(threshold=sys.maxsize)
 
