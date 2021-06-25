1a6a0a38db21eeb75c2083038801b4ae0deee665
andrew-j-millward <andrew.j.millward@gmail.com>
2021-06-24 23:47:24 -0400

    Fowlkes Mallows Score and other metrics

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -2,9 +2,14 @@ import sys
 sys.path.append('../L2Unifrac')
 sys.path.append('../L2Unifrac/src')
 sys.path.append('../src')
-import csv
+import csv, itertools
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics.cluster import adjusted_rand_score
+from sklearn.metrics.cluster import normalized_mutual_info_score
+from sklearn.metrics.cluster import adjusted_mutual_info_score
+from sklearn.metrics.cluster import fowlkes_mallows_score
+
 from sklearn_extra.cluster import KMedoids
 import BiomWrapper as BW
 import metadata_wrapper as meta
@@ -16,7 +21,7 @@ if __name__ == "__main__":
 	if len(args) > 3:
 		raise Exception("Invalid number of parameters.")
 	elif len(args) == 1:
-		file = 'L2-UniFrac-Out.csv'
+		file = 'L1-UniFrac-Out.csv'
 		num_clusters = 5
 	elif len(args) == 2:
 		file = args[1]
@@ -32,6 +37,8 @@ if __name__ == "__main__":
 		distance_matrix.append(list(map(float, i[0].split(","))))
 
 	cluster = AgglomerativeClustering(n_clusters=num_clusters, affinity='precomputed', linkage='complete').fit_predict(distance_matrix)  
+
+	#cluster = KMedoids(n_clusters=num_clusters, metric='precomputed', method='pam', init='heuristic').fit_predict(distance_matrix)
 
 	print(cluster)
 
@@ -52,41 +59,83 @@ if __name__ == "__main__":
 	print(cluster)
 	print(PCoA_Samples)
 
-	count_dict = [[0 for j in range(num_clusters)] for i in range(num_clusters)]
-	for i in range(len(cluster)):
-		count_dict[cluster[i]][PCoA_Samples[i]] += 1
+	permutations = list(itertools.permutations(range(num_clusters), num_clusters))
+	correct_max = 0
+	correct_max_i = 0
+	for i in range(len(permutations)):
+		correct = 0
+		set_map = {j:permutations[i][j] for j in range(num_clusters)}
+		for j in range(len(cluster)):
+			if set_map[cluster[j]] == PCoA_Samples[j]:
+				correct += 1
+		if correct > correct_max:
+			correct_max = correct
+			correct_max_i = i
+	print(correct_max, correct_max/len(cluster), permutations[correct_max_i])
+	# L1: 0.49217075984836
+	# L2: 0.47651227954507996
 
-	disallowed_set = []
-	disallowed_match = []
-	translation_dict = {}
-	for iteration in range(num_clusters):
-		print(count_dict)
-		most_probable_set = None
-		most_probable_match = None
-		for i in range(num_clusters):
-			for j in range(num_clusters):
-				if most_probable_set == None and most_probable_match == None and i not in disallowed_set and j not in disallowed_match:
-					most_probable_set = i
-					most_probable_match = j
-				elif i not in disallowed_set and j not in disallowed_match:
-					if count_dict[i][j]/sum(count_dict[i]) > count_dict[most_probable_set][most_probable_match]/sum(count_dict[most_probable_set]) and i not in disallowed_set and j not in disallowed_match:
-						most_probable_set = i
-						most_probable_match = j
-		print(count_dict[most_probable_set][most_probable_match]/sum(count_dict[most_probable_set]))
-		disallowed_set.append(most_probable_set)
-		disallowed_match.append(most_probable_match)
-		translation_dict[most_probable_set] = most_probable_match
-		print(most_probable_set, most_probable_match, disallowed_match) # 46.3%, 84.9%, 41.4%, 76.7%, 81.5%
+	score = adjusted_rand_score(PCoA_Samples, cluster)
+	print(score)
+	score2 = normalized_mutual_info_score(PCoA_Samples, cluster)
+	print(score2)
+	score3 = adjusted_mutual_info_score(PCoA_Samples, cluster)
+	print(score3)
+	score4 = fowlkes_mallows_score(PCoA_Samples, cluster)
+	print(score4)
 
-	num_correct = 0
-	num_incorrect = 0
-	for i in range(len(cluster)):
-		cluster[i] = translation_dict[cluster[i]]
-		if cluster[i] == PCoA_Samples[i]:
-			num_correct += 1
-		else:
-			num_incorrect += 1
+	# L2:
+	# 0.12190268522506367
+	# 0.22313556847817295
+	# 0.2222487106613287
+	# 0.45534086122442907
 
-	print(cluster)
-	print(num_correct, num_incorrect)
-	print(num_correct/len(cluster)) #0.47008406131531233 - L1, 0.4720619746167793 - L2
+	#count_dict = [[0 for j in range(num_clusters)] for i in range(num_clusters)]
+	#for i in range(len(cluster)):
+	#	count_dict[cluster[i]][PCoA_Samples[i]] += 1
+
+	#disallowed_set = []
+	#disallowed_match = []
+	#translation_dict = {}
+	#for iteration in range(num_clusters):
+	#	print(count_dict)
+	#	most_probable_set = None
+	#	most_probable_match = None
+	#	for i in range(num_clusters):
+	#		for j in range(num_clusters):
+	#			if most_probable_set == None and most_probable_match == None and i not in disallowed_set and j not in disallowed_match:
+	#				most_probable_set = i
+	#				most_probable_match = j
+	#			elif i not in disallowed_set and j not in disallowed_match:
+	#				if count_dict[i][j]/sum(count_dict[i]) > count_dict[most_probable_set][most_probable_match]/sum(count_dict[most_probable_set]) and i not in disallowed_set and j not in disallowed_match:
+	#					most_probable_set = i
+	#					most_probable_match = j
+	#	print(count_dict[most_probable_set][most_probable_match]/sum(count_dict[most_probable_set]))
+	#	disallowed_set.append(most_probable_set)
+	#	disallowed_match.append(most_probable_match)
+	#	translation_dict[most_probable_set] = most_probable_match
+	#	print(most_probable_set, most_probable_match, disallowed_match) # 46.3%, 84.9%, 41.4%, 76.7%, 81.5%
+
+	#num_correct = 0
+	#num_incorrect = 0
+	#for i in range(len(cluster)):
+	#	cluster[i] = translation_dict[cluster[i]]
+	#	if cluster[i] == PCoA_Samples[i]:
+	#		num_correct += 1
+	#	else:
+	#		num_incorrect += 1
+
+	#print(cluster)
+	#print(num_correct, num_incorrect)
+	#print(num_correct/len(cluster)) 
+
+	# AgglomerativeClustering
+	# 0.47008406131531233 - L1, 0.4720619746167793 - L2
+
+	# KMedoids
+	# 0.6245261249381903 - L1, 0.575243118509972 - L2
+
+	#kmedoids = KMedoids(n_clusters=5, metric='precomputed', method='pam', init='heuristic').fit_predict(distance_matrix)
+	#labels = kmedoids.labels_
+
+	#print(kmedoids)
