e86de988b5c542fcf426a65eb57842970f7ae053
andrew-j-millward <andrew.j.millward@gmail.com>
2021-07-25 19:39:51 -0400

    Updated preproprocessing to be a function

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -8,43 +8,61 @@ import BiomWrapper as BW
 import CSVWrapper as CSV
 import multiprocessing as mp
 
-cores = mp.cpu_count()
+def generate_preprocessed(biom_file, tree_file, output_file_L1=None, output_file_L2=None):
+	cores = mp.cpu_count()
 
-# Note: these are the same for L1/L2, so they will be computed only once.
-nodes_samples = BW.extract_biom('../data/47422_otu_table.biom')
-T1, l1, nodes_in_order = L2U.parse_tree_file('../data/trees/gg_13_5_otus_99_annotated.tree')
-(nodes_weighted, samples_temp) = L2U.parse_envs(nodes_samples, nodes_in_order)
+	# Note: these are the same for L1/L2, so they will be computed only once.
+	nodes_samples = BW.extract_biom(biom_file)
+	T1, l1, nodes_in_order = L2U.parse_tree_file(tree_file)
+	(nodes_weighted, samples_temp) = L2U.parse_envs(nodes_samples, nodes_in_order)
 
-PCoA_Samples = BW.extract_samples('../data/47422_otu_table.biom')
+	PCoA_Samples = BW.extract_samples(biom_file)
 
-def L1_pushup_worker(sample_num):
-	L1_Pushed = L1U.push_up(nodes_weighted[PCoA_Samples[sample_num]], T1, l1, nodes_in_order)
-	return L1_Pushed
+	def L1_pushup_worker(sample_num):
+		L1_Pushed = L1U.push_up(nodes_weighted[PCoA_Samples[sample_num]], T1, l1, nodes_in_order)
+		return L1_Pushed
 
-def L2_pushup_worker(sample_num):
-	L2_Pushed = L2U.push_up(nodes_weighted[PCoA_Samples[sample_num]], T1, l1, nodes_in_order)
-	return L2_Pushed
+	def L2_pushup_worker(sample_num):
+		L2_Pushed = L2U.push_up(nodes_weighted[PCoA_Samples[sample_num]], T1, l1, nodes_in_order)
+		return L2_Pushed
 
-# Multi Core Method
-values = range(len(PCoA_Samples))
+	# Multi Core Method
+	L1_preprocessed = []
+	L2_preprocessed = []
 
-dim1 = len(PCoA_Samples)
-dim2 = len(L1_pushup_worker(0))
-CSV.write('L1-Push-Out.csv', [dim1, dim2])
-CSV.write('L2-Push-Out.csv', [dim1, dim2])
+	values = range(len(PCoA_Samples))
 
-with mp.Pool(processes=int(cores/2-2)) as pool:
-	L1_result = pool.map(L1_pushup_worker, values)
+	dim1 = len(PCoA_Samples)
+	dim2 = len(L1_pushup_worker(0))
 
-for i in range(len(L1_result)):
-	for j in range(len(L1_result[i])):
-		if L1_result[i][j] != 0:
-			CSV.write('L1-Push-Out.csv', [i, j, L1_result[i][j]])
+	if output_file_L1 is not None:
+		CSV.write(output_file_L1, [dim1, dim2])
+	L1_preprocessed.append([dim1, dim2])
+	if output_file_L2 is not None:
+		CSV.write(output_file_L2, [dim1, dim2])
+	L2_preprocessed.append([dim1, dim2])
 
-with mp.Pool(processes=int(cores/2-2)) as pool:
-	L2_result = pool.map(L2_pushup_worker, values)
+	with mp.Pool(processes=int(cores/2-2)) as pool:
+		L1_result = pool.map(L1_pushup_worker, values)
 
-for i in range(len(L2_result)):
-	for j in range(len(L2_result[i])):
-		if L2_result[i][j] != 0:
-			CSV.write('L2-Push-Out.csv', [i, j, L2_result[i][j]])
+	for i in range(len(L1_result)):
+		for j in range(len(L1_result[i])):
+			if L1_result[i][j] != 0:
+				if output_file_L1 is not None:
+					CSV.write(output_file_L1, [i, j, L1_result[i][j]])
+				L1_preprocessed.append([i, j, L1_result[i][j]])
+
+	with mp.Pool(processes=int(cores/2-2)) as pool:
+		L2_result = pool.map(L2_pushup_worker, values)
+
+	for i in range(len(L2_result)):
+		for j in range(len(L2_result[i])):
+			if L2_result[i][j] != 0:
+				if output_file_L2 is not None:
+					CSV.write(output_file_L2, [i, j, L2_result[i][j]])
+				L2_preprocessed.append([i, j, L2_result[i][j]])
+
+	return L1_preprocessed, L2_preprocessed
+
+if __name__ == '__main__':
+	generate_preprocessed('../data/47422_otu_table.biom', '../data/trees/gg_13_5_otus_99_annotated.tree', 'L1-Push-Out.csv', 'L2-Push-Out.csv')
