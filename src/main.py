533f8b49739398cf3bfbfe917d23c21c8911db4c
andrew-j-millward <andrew.j.millward@gmail.com>
2021-07-26 09:57:10 -0400

    Fixed issue for run test

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ import L1_pairwise_unifrac as pairwise1
 import pairwise_unifrac as pairwise2
 import matplotlib.pyplot as plt
 import MetadataWrapper as meta
-import averages.compute_averages as avg
+import averages as avg
 import clustering as cluster
 
 import numpy as np
@@ -39,7 +39,7 @@ def generate_group_pcoa(biom_file, tree_file, metadata_file, tax_file):
 			groups.append(groups_temp[i]['body_site'])
 	group_str = ','.join(groups)
 	L1_preprocessed, L2_preprocessed = generate_preprocessed(biom_file, tree_file)
-	_, _, _, _, _, _, _, _, L1_distance_matrix, L2_distance_matrix, _, _ = avg(L1_preprocessed, L2_preprocessed, biom_file, tree_file, metadata_file, tax_file)
+	_, _, _, _, _, _, _, _, L1_distance_matrix, L2_distance_matrix, _, _ = avg.compute_averages(L1_preprocessed, L2_preprocessed, biom_file, tree_file, metadata_file, tax_file)
 	pcoa_out_L1 = pcoa.PCoA_group_from_matrix(L1_distance_matrix, biom_file, group_str, plot=False)
 	plt.savefig('images/out_L1_group_average.png')
 	pcoa_out_L2 = pcoa.PCoA_group_from_matrix(L2_distance_matrix, biom_file, group_str, plot=False)
@@ -51,5 +51,5 @@ def generate_clustering_report(biom_file, tree_file, metadata_file):
 	cluster.report_clustering(total_matrix_L1, total_matrix_L2, biom_file, metadata_file, 'reports/clustering_report.txt')
 
 if __name__ == '__main__':
-	generate_group_pcoa('../data/47422_otu_table.biom', '../data/trees/gg_13_5_otus_99_annotated.tree', '../data/metadata/P_1928_65684500_raw_meta.txt', '../data/taxonomies/gg_13_8_99.gg.tax')
-	#generate_total_pcoa('../data/47422_otu_table.biom', '../data/trees/gg_13_5_otus_99_annotated.tree', '../data/metadata/P_1928_65684500_raw_meta.txt')
+	#generate_group_pcoa('../data/47422_otu_table.biom', '../data/trees/gg_13_5_otus_99_annotated.tree', '../data/metadata/P_1928_65684500_raw_meta.txt', '../data/taxonomies/gg_13_8_99.gg.tax')
+	generate_total_pcoa('../data/47422_otu_table.biom', '../data/trees/gg_13_5_otus_99_annotated.tree', '../data/metadata/P_1928_65684500_raw_meta.txt')
