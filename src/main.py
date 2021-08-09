28ae8c75cb53930f4425680cab43232f6709e358
andrew-j-millward <andrew.j.millward@gmail.com>
2021-08-09 00:10:26 -0400

    Added more parser args

--- src/main.py
+++ src/main.py
@@ -60,15 +60,28 @@ if __name__ == '__main__':
 		description="This script calculates various metrics using L2-UniFrac (or optionally L1 for comparison). "
 					"It runs requests E2E without the user needing to perform intermediate steps. ", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('-v', '--verbose', action="store_true", help="Print out progress report/timing information")
+	
 	parser.add_argument('-th', '--threads', type=int, metavar='', help="Number of threads to use",
 						default=mp.cpu_count())
+	
 	parser.add_argument('-i', '--intermediate_store', action="store_true", help="Stores intermediate report generation")
 	parser.add_argument('-p', '--preprocessed_use', action="store_true", help="Uses intermediate report data to accelerate output")
-	parser.add_argument('-b', '--biom_file', metavar='', help="Biom file: Sample dataset in the BIOM file format")
-	parser.add_argument('-t', '--tree_file', metavar='', help="Tree file: OTU Tree in TREE file format")
-	parser.add_argument('-m', '--metadata_file', metavar='', help="Metadata file: Sample names will be the first column and body habitats are the third column, following 'OBERON:'")
-	parser.add_argument('-x', '--taxonomy_file', metavar='', help="Taxonomy file: Conversion between Tree Node and Taxonomy")
-	parser.add_argument('-o', '--out_file', metavar='', help='Output csv file with the containment indices.')
+
+	parser.add_argument('-bi', '--biom_file', metavar='', help="Biom file: Sample dataset in the BIOM file format")
+	parser.add_argument('-tr', '--tree_file', metavar='', help="Tree file: OTU Tree in TREE file format")
+	parser.add_argument('-md', '--metadata_file', metavar='', help="Metadata file: Sample names will be the first column and body habitats are the third column, following 'OBERON:'")
+	parser.add_argument('-tx', '--taxonomy_file', metavar='', help="Taxonomy file: Conversion between Tree Node and Taxonomy")
+	
+	parser.add_argument('-o', '--out_file', metavar='', help='Output csv file with the containment indices')
+
+	parser.add_argument('-t', '--total_pcoa', action="store_true", help='Compute total PCoA and generate plot')
+	parser.add_argument('-g', '--group_pcoa', action="store_true", help='Compute group PCoA averages and generate plot')
+	parser.add_argument('-c', '--clustering_report', action="store_true", help='Compute clustering report for distance matrix')
+	
+	parser.add_argument('-L1', '--L1_UniFrac', action="store_true", help='Compute using L1 UniFrac only')
+	parser.add_argument('-L2', '--L2_UniFrac', action="store_true", help='Compute using L2 UniFrac only')
+	parser.add_argument('-U', '--UniFrac', action="store_true", help='Compute using both L1 and L2 UniFrac')
+
 	#generate_group_pcoa('../data/47422_otu_table.biom', '../data/trees/gg_13_5_otus_99_annotated.tree', '../data/metadata/P_1928_65684500_raw_meta.txt', '../data/taxonomies/gg_13_8_99.gg.tax')
 	#generate_total_pcoa('../data/47422_otu_table.biom', '../data/trees/gg_13_5_otus_99_annotated.tree', '../data/metadata/P_1928_65684500_raw_meta.txt')
 
