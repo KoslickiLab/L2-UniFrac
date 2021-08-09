d42c0025e4f1c733eb4069b544f684b08497da5e
andrew-j-millward <andrew.j.millward@gmail.com>
2021-08-09 15:44:48 -0400

    Total PCoA generation intermediate load and store and verbose printing completed

--- src/main.py
+++ src/main.py
@@ -8,7 +8,6 @@ from os import path
 import L1Unifrac as L1U
 import L2Unifrac as L2U
 import BiomWrapper as BW
-import CSVWrapper as CSV
 import TaxWrapper as tax
 
 import PCoA_analysis as pcoa
@@ -22,20 +21,73 @@ import argparse
 from argparse import ArgumentTypeError
 import multiprocessing as mp
 import time
+import CSVWrapper as CSV
 
 import numpy as np
 
 def generate_total_pcoa(biom_file, tree_file, metadata_file, verbose, threads, intermediate_store, preprocessed_use, unifrac_code):
+	print(biom_file, tree_file, metadata_file, verbose, threads, intermediate_store, preprocessed_use, unifrac_code)
+	return
 	if unifrac_code == 1 or unifrac_code == 2:
-		total_matrix_L1 = pairwise1.Total_Pairwise(biom_file, tree_file)
+		if preprocessed_use and path.exists('intermediate/generate_total_pcoa_L1_intermediate.txt'):
+			total_matrix_L1 = CSV.read('intermediate/generate_total_pcoa_L1_intermediate.txt')
+			if verbose:
+				print('\tSuccessfully retrieved intermediate file for L1 Generate Total PCoA')
+		else:
+			if verbose:
+				print('\tWarning: Intermediate selected but not available. Computing pairwise alignments... This may take a while...')
+			total_matrix_L1 = pairwise1.Total_Pairwise(biom_file, tree_file)
+			if verbose:
+				print('\tCompleted pairwise distance matrix computation')
+			if intermediate_store:
+				if verbose:
+					print('\tStoring pairwise alignments...')
+				if path.exists('intermediate/generate_total_pcoa_L1_intermediate.txt'):
+					os.remove('intermediate/generate_total_pcoa_L1_intermediate.txt')
+				for i in range(len(total_matrix_L1)):
+					CSV.write('intermediate/generate_total_pcoa_L1_intermediate.txt', total_matrix_L1[i])
+				if verbose:
+					print('\tL1 pairwise distance matrix stored successfully')
+		if verbose:
+			print('\tGenerating PCoA...')
 		pcoa_out_L1 = pcoa.PCoA_total_from_matrix(total_matrix_L1, biom_file, metadata_file)
+		if verbose:
+			print('\tGeneration complete. Saving...')
 		plt.savefig('images/out_L1.png')
+		if verbose:
+			print('\tL1 PCoA successfully saved')
 	if unifrac_code == 0 or unifrac_code == 1:
-		total_matrix_L2 = pairwise2.Total_Pairwise(biom_file, tree_file)
-		pcoa_out_L1 = pcoa.PCoA_total_from_matrix(total_matrix_L2, biom_file, metadata_file)
+		if preprocessed_use and path.exists('intermediate/generate_total_pcoa_L2_intermediate.txt'):
+			total_matrix_L2 = CSV.read('intermediate/generate_total_pcoa_L2_intermediate.txt')
+			if verbose:
+				print('\tSuccessfully retrieved intermediate file for L2 Generate Total PCoA')
+		else:
+			if verbose:
+				print('\tWarning: Intermediate selected but not available. Computing pairwise distance matrix... This may take a while...')
+			total_matrix_L2 = pairwise2.Total_Pairwise(biom_file, tree_file)
+			if verbose:
+				print('\tCompleted pairwise distance matrix computation')
+			if intermediate_store:
+				if verbose:
+					print('\tStoring pairwise alignments...')
+				if path.exists('intermediate/generate_total_pcoa_L2_intermediate.txt'):
+					os.remove('intermediate/generate_total_pcoa_L2_intermediate.txt')
+				for i in range(len(total_matrix_L2)):
+					CSV.write('intermediate/generate_total_pcoa_L2_intermediate.txt', total_matrix_L2[i])
+				if verbose:
+					print('\tL2 pairwise distance matrix stored successfully')
+		if verbose:
+			print('\tGenerating PCoA...')
+		pcoa_out_L2 = pcoa.PCoA_total_from_matrix(total_matrix_L2, biom_file, metadata_file)
+		if verbose:
+			print('\tGeneration complete. Saving...')
 		plt.savefig('images/out_L2.png')
+		if verbose:
+			print('\tL2 PCoA successfully saved')
 
 def generate_group_pcoa(biom_file, tree_file, metadata_file, tax_file, verbose, threads, intermediate_store, preprocessed_use, unifrac_code):
+	print(biom_file, tree_file, metadata_file, tax_file, verbose, threads, intermediate_store, preprocessed_use, unifrac_code)
+	return
 	metadata = meta.extract_metadata(metadata_file)
 	sample_groups = []
 	groups_temp = list(metadata.values())
@@ -52,35 +104,37 @@ def generate_group_pcoa(biom_file, tree_file, metadata_file, tax_file, verbose, 
 	plt.savefig('images/out_L2_group_average.png')
 
 def generate_clustering_report(biom_file, tree_file, metadata_file, verbose, threads, intermediate_store, preprocessed_use, unifrac_code):
+	print(biom_file, tree_file, metadata_file, verbose, threads, intermediate_store, preprocessed_use, unifrac_code)
+	return
 	total_matrix_L1 = pairwise1.Total_Pairwise(biom_file, tree_file)
 	total_matrix_L2 = pairwise2.Total_Pairwise(biom_file, tree_file)
 	cluster.report_clustering(total_matrix_L1, total_matrix_L2, biom_file, metadata_file, 'reports/clustering_report.txt')
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
-		description="This script calculates various metrics using L2-UniFrac (or optionally L1 for comparison). "
-					"It runs requests E2E without the user needing to perform intermediate steps. ", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument('-v', '--verbose', action="store_true", help="Print out progress report/timing information")
+		description='This script calculates various metrics using L2-UniFrac (or optionally L1 for comparison). '
+					'It runs requests E2E without the user needing to perform intermediate steps. ', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('-v', '--verbose', action='store_true', help='Print out progress report/timing information')
 	
-	parser.add_argument('-th', '--threads', type=int, metavar='', help="Number of threads to use",
+	parser.add_argument('-th', '--threads', type=int, metavar='', help='Number of threads to use',
 						default=int(mp.cpu_count()/4))
 	
-	parser.add_argument('-i', '--intermediate_store', action="store_true", help="Stores intermediate report generation")
-	parser.add_argument('-p', '--preprocessed_use', action="store_true", help="Uses intermediate report data to accelerate output")
+	parser.add_argument('-i', '--intermediate_store', action='store_true', help='Stores intermediate report generation')
+	parser.add_argument('-p', '--preprocessed_use', action='store_true', help='Uses intermediate report data to accelerate output')
 
-	parser.add_argument('-bi', '--biom_file', metavar='', help="Biom file: Sample dataset in the BIOM file format")
-	parser.add_argument('-tr', '--tree_file', metavar='', help="Tree file: OTU Tree in TREE file format")
-	parser.add_argument('-md', '--metadata_file', metavar='', help="Metadata file: Sample names will be the first column and body habitats are the third column, following 'OBERON:'")
-	parser.add_argument('-tx', '--taxonomy_file', metavar='', help="Taxonomy file: Conversion between Tree Node and Taxonomy")
+	parser.add_argument('-bi', '--biom_file', metavar='', help='Biom file: Sample dataset in the BIOM file format')
+	parser.add_argument('-tr', '--tree_file', metavar='', help='Tree file: OTU Tree in TREE file format')
+	parser.add_argument('-md', '--metadata_file', metavar='', help='Metadata file: Sample names will be the first column and body habitats are the third column, following 'OBERON:'')
+	parser.add_argument('-tx', '--taxonomy_file', metavar='', help='Taxonomy file: Conversion between Tree Node and Taxonomy')
 	
 	parser.add_argument('-o', '--out_file', metavar='', help='Generic output file name. Specific requested operations will append tags and filetypes to the end')
 
-	parser.add_argument('-t', '--total_pcoa', action="store_true", help='Compute total PCoA and generate plot')
-	parser.add_argument('-g', '--group_pcoa', action="store_true", help='Compute group PCoA averages and generate plot')
-	parser.add_argument('-c', '--clustering_report', action="store_true", help='Compute clustering report for distance matrix')
+	parser.add_argument('-t', '--total_pcoa', action='store_true', help='Compute total PCoA and generate plot')
+	parser.add_argument('-g', '--group_pcoa', action='store_true', help='Compute group PCoA averages and generate plot')
+	parser.add_argument('-c', '--clustering_report', action='store_true', help='Compute clustering report for distance matrix')
 	
-	parser.add_argument('-L1', '--L1_UniFrac', action="store_true", help='Compute using L1 UniFrac only')
-	parser.add_argument('-U', '--UniFrac', action="store_true", help='Compute using both L1 and L2 UniFrac')
+	parser.add_argument('-L1', '--L1_UniFrac', action='store_true', help='Compute using L1 UniFrac only')
+	parser.add_argument('-U', '--UniFrac', action='store_true', help='Compute using both L1 and L2 UniFrac')
 
 	#generate_group_pcoa('../data/47422_otu_table.biom', '../data/trees/gg_13_5_otus_99_annotated.tree', '../data/metadata/P_1928_65684500_raw_meta.txt', '../data/taxonomies/gg_13_8_99.gg.tax')
 	#generate_total_pcoa('../data/47422_otu_table.biom', '../data/trees/gg_13_5_otus_99_annotated.tree', '../data/metadata/P_1928_65684500_raw_meta.txt')
@@ -97,6 +151,8 @@ if __name__ == '__main__':
 		raise Exception('Error: Invalid Metadata File Path')
 	elif ((isinstance(args.taxonomy_file, str) and not path.exists(args.taxonomy_file)) or args.taxonomy_file is None) and (args.group_pcoa):
 		raise Exception('Error: Invalid Taxonomy File Path')
+	elif args.out_file is None and (args.total_pcoa or args.group_pcoa or args.clustering_report):
+		raise Exception('Error: Missing output file subname. Please specify and continue')
 
 	unifrac_code = 0
 	if args.UniFrac:
@@ -113,18 +169,18 @@ if __name__ == '__main__':
 			print('Starting Total PCoA Generation...')
 		generate_total_pcoa(args.biom_file, args.tree_file, args.metadata_file, args.verbose, args.threads, args.intermediate_store, args.preprocessed_use, unifrac_code)
 		if args.verbose:
-			print('Total PCoA Generation Complete. Total Elapsed Time: ' + str(time.time()-segment_start))
+			print('Total PCoA Generation Complete. Total Elapsed Time: ' + str(time.time()-segment_start) + ' seconds')
 	if args.group_pcoa:
 		segment_start = time.time()
 		if args.verbose:
 			print('Starting Group PCoA Generation...')
 		generate_group_pcoa(args.biom_file, args.tree_file, args.metadata_file, args.taxonomy_file, args.verbose, args.threads, args.intermediate_store, args.preprocessed_use, unifrac_code)
 		if args.verbose:
-			print('Group PCoA Generation Complete. Total Elapsed Time: ' + str(time.time()-segment_start))
+			print('Group PCoA Generation Complete. Total Elapsed Time: ' + str(time.time()-segment_start) + ' seconds')
 	if args.clustering_report:
 		segment_start = time.time()
 		if args.verbose:
 			print('Starting Clustering Report Generation...')
 		generate_clustering_report(args.biom_file, args.tree_file, args.metadata_file, args.verbose, args.threads, args.intermediate_store, args.preprocessed_use, unifrac_code)
 		if args.verbose:
-			print('Clustering Report Generation Complete. Total Elapsed Time: ' + str(time.time()-segment_start))
+			print('Clustering Report Generation Complete. Total Elapsed Time: ' + str(time.time()-segment_start) + ' seconds')
