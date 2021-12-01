Fixed old TODO. Individual average functions

scripts/averages.py
@@ -28,6 +28,7 @@ from scipy.sparse import csr_matrix, lil_matrix
 # are very large relative to these erroneous ones.
 negatives_filtering_threshold = -10e-14
 
+# Computes the pairwise average
 def compute_pairwise_pushed_L1(pushed_arr):
 	dist_matrix = []
 	for i in range(len(pushed_arr)):
@@ -37,6 +38,8 @@ def compute_pairwise_pushed_L1(pushed_arr):
 			dist_arr.append(unifrac_distance)
 		dist_matrix.append(dist_arr)
 	return dist_matrix
+
+# Computes the pairwise average
 def compute_pairwise_pushed_L2(pushed_arr):
 	dist_matrix = []
 	for i in range(len(pushed_arr)):
@@ -48,16 +51,16 @@ def compute_pairwise_pushed_L2(pushed_arr):
 	return dist_matrix
 
 # Helper function for averages. Outputs a CSV containing info from each step and negative counts at end.
-def compute_averages(distance_obj, biom_file, tree_file, metadata_file, tax_file, output_file=None, unifrac_code=0):
+def compute_L1_averages(L1_file, biom_file, tree_file, metadata_file, tax_file, output_file=None):
 	
 	if path.exists(output_file):
 		os.remove(output_file)
 
-	if unifrac_code == 1 or unifrac_code == 2:
-		T1, l1, nodes_in_order = L1U.parse_tree_file(tree_file)
-	elif unifrac_code == 0 or unifrac_code == 1:
-		T1, l1, nodes_in_order = L2U.parse_tree_file(tree_file)
+	# Note: these are the same for L1/L2, so they will be computed only once. (USE T1 FOR ANCESTORS FOR TEMP NODES)
+	#nodes_samples = BW.extract_biom(biom_file)
+	T1, l1, nodes_in_order = L1U.parse_tree_file(tree_file)
 
+	# Subsample Biom file (2 samples). Then trace the mass to find where it no longer sums to 1.
 	PCoA_Samples = BW.extract_samples(biom_file)
 	metadata = meta.extract_metadata(metadata_file)
 
@@ -72,16 +75,12 @@ def compute_averages(distance_obj, biom_file, tree_file, metadata_file, tax_file
 		PCoA_Samples[i] = region_names.index(metadata[PCoA_Samples[i]]['body_site'])
 
 	# Read sparse matrices
-	if not isinstance(distance_obj, list):
-		sparse_matrix = CSV.read_sparse(distance_obj)
+	if not isinstance(L1_file, list):
+		sparse_matrix_L1 = CSV.read_sparse(L1_file)
 	else:
-		sparse_matrix = lil_matrix((int(distance_obj[0][0]), int(distance_obj[0][1])))
-		for i in range(len(distance_obj)):
-			if len(distance_obj[i]) > 2:
-				sparse_matrix[(distance_obj[i][0], distance_obj[i][1])] = distance_obj[i][2]
-		sparse_matrix = csr_matrix(sparse_matrix)
+		sparse_matrix_L1 = L1_file
 
-	group_averages = {}
+	group_averages_L1 = {}
 
 	# Store region names for later
 	if output_file is not None:
@@ -110,60 +109,44 @@ def compute_averages(distance_obj, biom_file, tree_file, metadata_file, tax_file
 					else:
 						tax_arr.append('root')
 						break
-
+	
 	if output_file is not None:
 		CSV.write(output_file, tax_arr)
 
 	# Take L1 average of each
-	pushed_arr = []
+	L1_pushed_arr = []
 	for i in range(len(region_names)):
 		group_arr = []
 		for j in range(len(region_map[region_names[i]])):
-			group_arr.append(np.array(sparse_matrix[region_map[region_names[i]][j]].todense())[0])
-		if unifrac_code == 1 or unifrac_code == 2:
-			average = L1U.median_of_vectors(group_arr)
-		if unifrac_code == 0 or unifrac_code == 1:
-			average = L2U.mean_of_vectors(group_arr)
-		group_averages[region_names[i]] = average
-		pushed_arr.append(average)
+			group_arr.append(np.array(sparse_matrix_L1[region_map[region_names[i]][j]].todense())[0])
+		average = L1U.median_of_vectors(group_arr)
+		group_averages_L1[region_names[i]] = average
+		L1_pushed_arr.append(average)
 
 	# Store L1 averages
-	if unifrac_code == 1 or unifrac_code == 2:
-		print("L1 Group Averages:")
-		if output_file is not None:
-			CSV.write(output_file, ["L1 Group Averages:"])
-	if unifrac_code == 0 or unifrac_code == 1:
-		print("L2 Group Averages:")
-		if output_file is not None:
-			CSV.write(output_file, ["L2 Group Averages:"])
+	print("L1 Group Averages:")
+	if output_file is not None:
+		CSV.write(output_file, ["L1 Group Averages:"])
 	for name in region_names:
 		padded_name = "{:<15}".format(name+":")
-		print(f"{padded_name} {group_averages[name]}")
+		print(f"{padded_name} {group_averages_L1[name]}")
 		if output_file is not None:
-			CSV.write(output_file, group_averages[name])
+			CSV.write(output_file, group_averages_L1[name])
 
 	# Push L1 down and store
-	if unifrac_code == 1 or unifrac_code == 2:
-		print("\nL1 Inverse Push Up:")
-		if output_file is not None:
-			CSV.write(output_file, ["L1 Pushed Up:"])
-	if unifrac_code == 0 or unifrac_code == 1:
-		print("\nL2 Inverse Push Up:")
-		if output_file is not None:
-			CSV.write(output_file, ["L2 Pushed Up:"])
-	neg_arr = []
-	inverse_pushed = {}
+	print("\nL1 Inverse Push Up:")
+	if output_file is not None:
+		CSV.write(output_file, ["L1 Pushed Up:"])
+	L1_neg_arr = []
+	L1_inverse_pushed = {}
 	for name in region_names:
 		neg_count = 0
-		if unifrac_code == 1 or unifrac_code == 2:
-			median_inverse = L1U.inverse_push_up(group_averages[name], T1, l1, nodes_in_order)
-		if unifrac_code == 0 or unifrac_code == 1:
-			median_inverse = L2U.inverse_push_up(group_averages[name], T1, l1, nodes_in_order)
-		inverse_pushed[name] = median_inverse
+		median_inverse = L1U.inverse_push_up(group_averages_L1[name], T1, l1, nodes_in_order)
+		L1_inverse_pushed[name] = median_inverse
 		for i in range(len(median_inverse)):
 			if median_inverse[i] < negatives_filtering_threshold:
 				neg_count += 1
-		neg_arr.append(neg_count)
+		L1_neg_arr.append(neg_count)
 		padded_name = "{:<15}".format(name+":")
 		print(f"{padded_name} {median_inverse}")
 		if output_file is not None:
@@ -171,38 +154,25 @@ def compute_averages(distance_obj, biom_file, tree_file, metadata_file, tax_file
 
 	# Write negative counts
 	if output_file is not None:
-		CSV.write(output_file, ["Negatives by Group:"])
-		CSV.write(output_file, neg_arr)
+		CSV.write(output_file, ["L1 Negatives by Group:"])
+		CSV.write(output_file, L1_neg_arr)
 
-	if unifrac_code == 1 or unifrac_code == 2:
-		distance_matrix = compute_pairwise_pushed_L1(pushed_arr)
-	if unifrac_code == 0 or unifrac_code == 1:
-		distance_matrix = compute_pairwise_pushed_L2(pushed_arr)
+	L1_distance_matrix = compute_pairwise_pushed_L1(L1_pushed_arr)
 
-	if unifrac_code == 1 or unifrac_code == 2:
-		print("L1 Distance Matrix:")
+	print("L1 Distance Matrix:")
+	if output_file is not None:
+		CSV.write(output_file, ["L1 Distance Matrix:"])
+	for i in range(len(L1_pushed_arr)):
+		print(L1_distance_matrix[i])
 		if output_file is not None:
-			CSV.write(output_file, ["L1 Distance Matrix:"])
-	if unifrac_code == 0 or unifrac_code == 1:
-		print("L2 Distance Matrix:")
-		if output_file is not None:
-			CSV.write(output_file, ["L2 Distance Matrix:"])
-	for i in range(len(pushed_arr)):
-		print(distance_matrix[i])
-		if output_file is not None:
-			CSV.write(output_file, distance_matrix[i])
+			CSV.write(output_file, L1_distance_matrix[i])	
 
-	if unifrac_code == 1 or unifrac_code == 2:
-		print("L1 Abundances by Node Type:")
-		if output_file is not None:
-			CSV.write(output_file, ["L1 Abundances by Node Type:"])
-	if unifrac_code == 0 or unifrac_code == 1:
-		print("L2 Abundances by Node Type:")
-		if output_file is not None:
-			CSV.write(output_file, ["L2 Abundances by Node Type:"])
-	node_type_group_abundances = []
+	print("L1 Abundances by Node Type:")
+	if output_file is not None:
+		CSV.write(output_file, ["L1 Abundances by Node Type:"])
+	L1_node_type_group_abundances = []
 	for name in region_names:
-		region_abundance_vector = inverse_pushed[name]
+		region_abundance_vector = L1_inverse_pushed[name]
 		k = p = c = o = f = g = s = temp = 0
 		for i in range(len(region_abundance_vector)):
 			node_tax = tax_arr[i].split(';')
@@ -228,9 +198,161 @@ def compute_averages(distance_obj, biom_file, tree_file, metadata_file, tax_file
 		print([k, p, c, o, f, g, s, temp])
 		if output_file is not None:
 			CSV.write(output_file, [k, p, c, o, f, g, s, temp])
-		node_type_group_abundances.append([k, p, c, o, f, g, s, temp])
+		L1_node_type_group_abundances.append([k, p, c, o, f, g, s, temp])
 
-	return region_names, tax_arr, group_averages, inverse_pushed, neg_arr, distance_matrix, node_type_group_abundances
+	return region_names, tax_arr, group_averages_L1, L1_inverse_pushed, L1_neg_arr, L1_distance_matrix, L1_node_type_group_abundances
+
+# Helper function for averages. Outputs a CSV containing info from each step and negative counts at end.
+def compute_L2_averages(L2_file, biom_file, tree_file, metadata_file, tax_file, output_file=None):
+
+	if path.exists(output_file):
+		os.remove(output_file)
+
+	# Note: these are the same for L1/L2, so they will be computed only once. (USE T1 FOR ANCESTORS FOR TEMP NODES)
+	#nodes_samples = BW.extract_biom(biom_file)
+	T1, l1, nodes_in_order = L2U.parse_tree_file(tree_file)
+
+	# Subsample Biom file (2 samples). Then trace the mass to find where it no longer sums to 1.
+	PCoA_Samples = BW.extract_samples(biom_file)
+	metadata = meta.extract_metadata(metadata_file)
+
+	# Extract region names and map samples to regions
+	region_names = []
+	region_map = {}
+	for i in range(len(PCoA_Samples)):
+		if metadata[PCoA_Samples[i]]['body_site'] not in region_names:
+			region_map[metadata[PCoA_Samples[i]]['body_site']] = []
+			region_names.append(metadata[PCoA_Samples[i]]['body_site'])
+		region_map[metadata[PCoA_Samples[i]]['body_site']].append(i)
+		PCoA_Samples[i] = region_names.index(metadata[PCoA_Samples[i]]['body_site'])
+
+	# Read sparse matrix
+	if not isinstance(L2_file, list):
+		sparse_matrix_L2 = CSV.read_sparse(L2_file)
+	else:
+		sparse_matrix_L2 = L2_file
+
+	group_averages_L2 = {}
+
+	# Store region names for later
+	if output_file is not None:
+		CSV.write(output_file, region_names)
+
+	# Write taxas for cell
+	taxonomies = tax.extract_tax(tax_file)
+	tax_arr = []
+	for i in range(len(nodes_in_order)):
+		if nodes_in_order[i][0] != 't':
+			tax_arr.append(taxonomies[int(nodes_in_order[i])])
+		else:
+			loop = True
+			if i in T1:
+				temp_node = T1[i]
+			else:
+				tax_arr.append('root')
+				loop = False
+			while loop:
+				if nodes_in_order[temp_node][0] != 't':
+					tax_arr.append(taxonomies[int(nodes_in_order[temp_node])])
+					break
+				else:
+					if temp_node in T1:
+						temp_node = T1[temp_node]
+					else:
+						tax_arr.append('root')
+						break
+	
+	if output_file is not None:
+		CSV.write(output_file, tax_arr)
+
+	# Take L2 average of each
+	L2_pushed_arr = []
+	for i in range(len(region_names)):
+		group_arr = []
+		for j in range(len(region_map[region_names[i]])):
+			group_arr.append(np.array(sparse_matrix_L2[region_map[region_names[i]][j]].todense())[0])
+		average = L2U.mean_of_vectors(group_arr)
+		group_averages_L2[region_names[i]] = average
+		L2_pushed_arr.append(average)
+
+	# Store L2 averages
+	print("\nL2 Group Averages:")
+	if output_file is not None:
+		CSV.write(output_file, ["L2 Group Averages:"])
+	for name in region_names:
+		padded_name = "{:<15}".format(name+":")
+		print(f"{padded_name} {group_averages_L2[name]}")
+		if output_file is not None:
+			CSV.write(output_file, group_averages_L2[name])
+
+	# Push L2 down and store
+	print("\nL2 Inverse Push Up:")
+	if output_file is not None:
+		CSV.write(output_file, ["L2 Pushed Up:"])
+	L2_neg_arr = []
+	L2_inverse_pushed = {}
+	for name in region_names:
+		neg_count = 0
+		mean_inverse = L2U.inverse_push_up(group_averages_L2[name], T1, l1, nodes_in_order)
+		L2_inverse_pushed[name] = mean_inverse
+		for i in range(len(mean_inverse)):
+			if mean_inverse[i] < negatives_filtering_threshold:
+				neg_count += 1
+		L2_neg_arr.append(neg_count)
+		padded_name = "{:<15}".format(name+":")
+		print(f"{padded_name} {mean_inverse}")
+		if output_file is not None:
+			CSV.write(output_file, mean_inverse)
+
+	# Write negative counts
+	if output_file is not None:
+		CSV.write(output_file, ["L2 Negatives by Group:"])
+		CSV.write(output_file, L2_neg_arr)
+
+	L2_distance_matrix = compute_pairwise_pushed_L2(L2_pushed_arr)
+
+	print("L2 Distance Matrix:")
+	if output_file is not None:
+		CSV.write(output_file, ["L2 Distance Matrix:"])
+	for i in range(len(L2_pushed_arr)):
+		print(L2_distance_matrix[i])
+		if output_file is not None:
+			CSV.write(output_file, L2_distance_matrix[i])		
+
+	print("L2 Abundances by Node Type:")
+	if output_file is not None:
+		CSV.write(output_file, ["L2 Abundances by Node Type:"])
+	L2_node_type_group_abundances = []
+	for name in region_names:
+		region_abundance_vector = L2_inverse_pushed[name]
+		k = p = c = o = f = g = s = temp = 0
+		for i in range(len(region_abundance_vector)):
+			node_tax = tax_arr[i].split(';')
+			if len(node_tax) > 1:
+				if node_tax[-2][0] == 'k':
+					k += region_abundance_vector[i]
+				elif node_tax[-2][0] == 'p':
+					p += region_abundance_vector[i]
+				elif node_tax[-2][0] == 'c':
+					c += region_abundance_vector[i]
+				elif node_tax[-2][0] == 'o':
+					o += region_abundance_vector[i]
+				elif node_tax[-2][0] == 'f':
+					f += region_abundance_vector[i]
+				elif node_tax[-2][0] == 'g':
+					g += region_abundance_vector[i]
+				elif node_tax[-2][0] == 's':
+					s += region_abundance_vector[i]
+				else:
+					print("Error")
+			else:
+				temp += region_abundance_vector[i]
+		print([k, p, c, o, f, g, s, temp])
+		if output_file is not None:
+			CSV.write(output_file, [k, p, c, o, f, g, s, temp])
+		L2_node_type_group_abundances.append([k, p, c, o, f, g, s, temp])
+
+	return region_names, tax_arr, group_averages_L2, L2_inverse_pushed, L2_neg_arr, L2_distance_matrix, L2_node_type_group_abundances
 
 # Helper function for averages. Outputs a CSV containing info from each step and negative counts at end.
 def compute_L1_L2_averages(L1_file, L2_file, biom_file, tree_file, metadata_file, tax_file, output_file=None):
@@ -473,7 +595,8 @@ def compute_L1_L2_averages(L1_file, L2_file, biom_file, tree_file, metadata_file
 
 # Argument parsing
 if __name__ == "__main__":
-	compute_L1_L2_averages('L1-Push-Out.csv', 'L2-Push-Out.csv', '../data/47422_otu_table.biom', '../data/trees/gg_13_5_otus_99_annotated.tree', '../data/metadata/P_1928_65684500_raw_meta.txt', '../data/taxonomies/gg_13_8_99.gg.tax', 'Group-Averages.csv')
+	#compute_L1_L2_averages('L1-Push-Out.csv', 'L2-Push-Out.csv', '../data/47422_otu_table.biom', '../data/trees/gg_13_5_otus_99_annotated.tree', '../data/metadata/P_1928_65684500_raw_meta.txt', '../data/taxonomies/gg_13_8_99.gg.tax', 'Group-Averages.csv')
+	compute_L2_averages('L2-Push-Out.csv', '../data/47422_otu_table.biom', '../data/trees/gg_13_5_otus_99_annotated.tree', '../data/metadata/P_1928_65684500_raw_meta.txt', '../data/taxonomies/gg_13_8_99.gg.tax', 'Group-Averages-2.csv')
 	args = sys.argv
 	if len(args) != 8:
 		raise Exception("Invalid number of parameters.")

src/L2Unifrac.py
@@ -110,12 +110,12 @@ def L2Unifrac_weighted_flow(Tint, lint, nodes_in_order, P, Q):
 		for j in pos[i]:
 			for k in neg[i]:
 				if (j not in posremove) and (k not in negremove):
-					val = np.minimum(G[(i, j)], -G[(i, k)])
+					val = np.minimum(G[(i, j)], -G[(i, k)])**2
 					if val > 0:
 						F[(j, k)] = np.minimum(G[(i, j)], -G[(i, k)])
-						G[(i, j)] = G[(i, j)] - (val**2)
-						G[(i, k)] = G[(i, k)] + (val**2)
-						Z = Z + (w[j] + w[k]) * (val**2)
+						G[(i, j)] = G[(i, j)] - val
+						G[(i, k)] = G[(i, k)] + val
+						Z = Z + (w[j] + w[k]) * val
 					if G[(i, j)] == 0:
 						posremove.add(j)
 					if G[(i, k)] == 0:
@@ -136,7 +136,8 @@ def L2Unifrac_weighted_flow(Tint, lint, nodes_in_order, P, Q):
 				diffab[(i, Tint[i])] = lint[i, Tint[i]]*diffab[(i, Tint[i])]  # Captures DiffAbund, scales by length of edge JLMC
 			pos[Tint[i]] |= pos[i]
 			neg[Tint[i]] |= neg[i]
-	return (np.sqrt(Z), F, diffab)  # The returned flow and diffab are on the basis nodes_in_order and is given in sparse matrix dictionary format. eg {(0,0):.5,(1,2):.5}
+	Z = np.sqrt(Z)
+	return (Z, F, diffab)  # The returned flow and diffab are on the basis nodes_in_order and is given in sparse matrix dictionary format. eg {(0,0):.5,(1,2):.5}
 
 def L2Unifrac_weighted_plain(ancestors, edge_lengths, nodes_in_order, P, Q):
 	'''