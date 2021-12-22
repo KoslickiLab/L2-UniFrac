64a883e4086ad891f1549fad67c1d873fdc16c13
andrew-j-millward <andrew-j-millward@gmail.com>
2021-12-22 16:25:04 -0500

    Typo on savefig

--- scripts/diffabUtils.py
+++ scripts/diffabUtils.py
@@ -11,4 +11,4 @@ def generate_diffab(regions, region_averages, Tint, lint, nodes_in_order, output
 			if i < j:
 				L2_UniFrac2, DifferentialAbundance = L2U.L2Unifrac_weighted(Tint, lint, nodes_in_order, region_averages[regions[i]], region_averages[regions[j]])
 				fig = L2U.plot_diffab(nodes_in_order, DifferentialAbundance, regions[i], regions[j], plot_zeros=False, thresh=thresh, show=False)
-				plt.savefig('images/{0}_diffab_{1}_{2}.png'.format(output. regions[i], regions[j]))
+				plt.savefig('images/{0}_diffab_{1}_{2}.png'.format(output, regions[i], regions[j]))
