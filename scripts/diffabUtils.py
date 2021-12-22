979f5b3669f806cece303fa19c736db7efdb9575
andrew-j-millward <andrew-j-millward@gmail.com>
2021-12-22 16:10:55 -0500

    Dict indexing using names

--- scripts/diffabUtils.py
+++ scripts/diffabUtils.py
@@ -8,6 +8,6 @@ def generate_diffab(regions, region_averages, Tint, lint, nodes_in_order, output
 	for i in range(len(region_averages)):
 		for j in range(len(region_averages)):
 			if i < j:
-				L2_UniFrac2, DifferentialAbundance = L2U.L2Unifrac_weighted(Tint, lint, nodes_in_order, region_averages[i], region_averages[j])
+				L2_UniFrac2, DifferentialAbundance = L2U.L2Unifrac_weighted(Tint, lint, nodes_in_order, region_averages[regions[i]], region_averages[regions[j]])
 				fig = L2U.plot_diffab(nodes_in_order, DifferentialAbundance, regions[i], regions[j], plot_zeros=False, thresh=thresh, show=False)
 				plt.savefig('images/{0}_diffab_{1}_{2}.png'.format(output. regions[i], regions[j]))
