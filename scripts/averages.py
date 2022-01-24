e10d6ccd7ac6b2443e0b0142b832317561713767
andrew-j-millward <andrew.j.millward@gmail.com>
2022-01-24 11:58:03 -0500

    Add most shared taxonomic clustering to L1avg

--- scripts/averages.py
+++ scripts/averages.py
@@ -97,26 +97,123 @@ def compute_L1_averages(L1_file, biom_file, tree_file, metadata_file, tax_file, 
 	# Write taxas for cell
 	taxonomies = tax.extract_tax(tax_file)
 	tax_arr = []
-	for i in range(len(nodes_in_order)):
-		if nodes_in_order[i][0] != 't':
-			tax_arr.append(taxonomies[int(nodes_in_order[i])])
-		else:
-			loop = True
-			if i in T1:
-				temp_node = T1[i]
-			else:
-				tax_arr.append('internal')
-				loop = False
-			while loop:
-				if nodes_in_order[temp_node][0] != 't':
-					tax_arr.append(taxonomies[int(nodes_in_order[temp_node])])
-					break
+
+	if most_shared:
+		leaf_nodes = []
+		for i in range(len(nodes_in_order)):
+			if nodes_in_order[i][0] != 't':
+				leaf_nodes.append(i)
+		descendent_dict = {i:[] for i in range(len(nodes_in_order))}
+		for i in range(len(leaf_nodes)):
+			temp_node = leaf_nodes[i]
+			while True:
+				if temp_node in T1:
+					if temp_node != leaf_nodes[i]:
+						descendent_dict[temp_node].append(leaf_nodes[i])
+					temp_node = T1[temp_node]
 				else:
-					if temp_node in T1:
-						temp_node = T1[temp_node]
-					else:
-						tax_arr.append('internal')
+					if temp_node != leaf_nodes[i]:
+						descendent_dict[temp_node].append(leaf_nodes[i])
+					break
+		for i in range(len(nodes_in_order)):
+			descendent_taxonomy = []
+			for j in range(len(descendent_dict[i])):
+				descendent_taxonomy.append(taxonomies[int(nodes_in_order[descendent_dict[i][j]])])
+			if len(descendent_taxonomy) == 0:
+				tax_arr.append(taxonomies[int(nodes_in_order[i])])
+			else:
+				k_dict = {}
+				p_dict = {}
+				c_dict = {}
+				o_dict = {}
+				f_dict = {}
+				g_dict = {}
+				s_dict = {}
+				for j in range(len(descendent_taxonomy)):
+					tmp_taxa = descendent_taxonomy[j].split(';')[:-1]
+					for k in range(len(tmp_taxa)):
+						if tmp_taxa[k][0] == 'k' and tmp_taxa[k] not in k_dict:
+							k_dict[tmp_taxa[k]] = 1
+						elif tmp_taxa[k][0] == 'k':
+							k_dict[tmp_taxa[k]] += 1
+						if tmp_taxa[k][0] == 'p' and tmp_taxa[k] not in p_dict:
+							p_dict[tmp_taxa[k]] = 1
+						elif tmp_taxa[k][0] == 'p':
+							p_dict[tmp_taxa[k]] += 1
+						if tmp_taxa[k][0] == 'c' and tmp_taxa[k] not in c_dict:
+							c_dict[tmp_taxa[k]] = 1
+						elif tmp_taxa[k][0] == 'c':
+							c_dict[tmp_taxa[k]] += 1
+						if tmp_taxa[k][0] == 'o' and tmp_taxa[k] not in o_dict:
+							o_dict[tmp_taxa[k]] = 1
+						elif tmp_taxa[k][0] == 'o':
+							o_dict[tmp_taxa[k]] += 1
+						if tmp_taxa[k][0] == 'f' and tmp_taxa[k] not in f_dict:
+							f_dict[tmp_taxa[k]] = 1
+						elif tmp_taxa[k][0] == 'f':
+							f_dict[tmp_taxa[k]] += 1
+						if tmp_taxa[k][0] == 'g' and tmp_taxa[k] not in g_dict:
+							g_dict[tmp_taxa[k]] = 1
+						elif tmp_taxa[k][0] == 'g':
+							g_dict[tmp_taxa[k]] += 1
+						if tmp_taxa[k][0] == 's' and tmp_taxa[k] not in s_dict:
+							s_dict[tmp_taxa[k]] = 1
+						elif tmp_taxa[k][0] == 's':
+							s_dict[tmp_taxa[k]] += 1
+				shared_taxonomy = ''
+				for key, value in k_dict.items():
+					if value == len(descendent_taxonomy):
+						shared_taxonomy = shared_taxonomy+key
 						break
+				for key, value in p_dict.items():
+					if value == len(descendent_taxonomy):
+						shared_taxonomy = shared_taxonomy+';'+key
+						break
+				for key, value in c_dict.items():
+					if value == len(descendent_taxonomy):
+						shared_taxonomy = shared_taxonomy+';'+key
+						break
+				for key, value in o_dict.items():
+					if value == len(descendent_taxonomy):
+						shared_taxonomy = shared_taxonomy+';'+key
+						break
+				for key, value in f_dict.items():
+					if value == len(descendent_taxonomy):
+						shared_taxonomy = shared_taxonomy+';'+key
+						break
+				for key, value in g_dict.items():
+					if value == len(descendent_taxonomy):
+						shared_taxonomy = shared_taxonomy+';'+key
+						break
+				for key, value in s_dict.items():
+					if value == len(descendent_taxonomy):
+						shared_taxonomy = shared_taxonomy+';'+key
+						break
+				shared_taxonomy = shared_taxonomy+';'
+				if shared_taxonomy == ';':
+					shared_taxonomy = 'Root' # Root node will include taxonomy from Archaea and Bacteria, thus sharing nothing.
+				tax_arr.append(shared_taxonomy)
+	else:
+		for i in range(len(nodes_in_order)):
+			if nodes_in_order[i][0] != 't':
+				tax_arr.append(taxonomies[int(nodes_in_order[i])])
+			else:
+				loop = True
+				if i in T1:
+					temp_node = T1[i]
+				else:
+					tax_arr.append('internal')
+					loop = False
+				while loop:
+					if nodes_in_order[temp_node][0] != 't':
+						tax_arr.append(taxonomies[int(nodes_in_order[temp_node])])
+						break
+					else:
+						if temp_node in T1:
+							temp_node = T1[temp_node]
+						else:
+							tax_arr.append('internal')
+							break
 	
 	if output_file is not None:
 		CSV.write(output_file, tax_arr)
