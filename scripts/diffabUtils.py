b4c6fca802469ed89b7b67d574035421f091dfd9
andrew-j-millward <andrew.j.millward@gmail.com>
2022-02-07 00:44:26 -0500

    Shift mass towards first shared taxonomic name for plotting

--- scripts/diffabUtils.py
+++ scripts/diffabUtils.py
@@ -6,18 +6,28 @@ import matplotlib.pyplot as plt
 import L1Unifrac as L1U
 import L2Unifrac as L2U
 
-def generate_diffab(regions, region_averages, Tint, lint, nodes_in_order, taxonomy_in_order, output, thresh, maxDisp, includeTemp, L):
+def generate_diffab(regions, region_averages, Tint, lint, nodes_in_order, taxonomy_in_order, output, thresh, maxDisp, includeTemp, L, include_tmp_diffab=False):
+	if include_tmp_diffab:
+		for i in range(len(region_averages)):
+			tax_mass_dict = {}
+			for k in range(len(taxonomy_in_order)):
+				if taxonomy_in_order[k] not in tax_mass_dict:
+					tax_mass_dict[taxonomy_in_order[k]] = 0
+				tax_mass_dict[taxonomy_in_order[k]] += region_averages[regions[i]][k]
+			for k in range(len(taxonomy_in_order)):
+				region_averages[regions[i]][k] = tax_mass_dict[taxonomy_in_order[k]]
+				tax_mass_dict[taxonomy_in_order[k]] = 0
 	if L == 1:
 		for i in range(len(region_averages)):
 			for j in range(len(region_averages)):
 				if i < j:
-					L1_UniFrac, DifferentialAbundance = L1U.EMDUnifrac_weighted(Tint, lint, nodes_in_order, region_averages[regions[i]], region_averages[regions[j]], include_tmp_diffab=False)
+					L1_UniFrac, DifferentialAbundance = L1U.EMDUnifrac_weighted(Tint, lint, nodes_in_order, region_averages[regions[i]], region_averages[regions[j]], include_tmp_diffab=include_tmp_diffab)
 					fig = L2U.plot_diffab(nodes_in_order, taxonomy_in_order, DifferentialAbundance, regions[i], regions[j], plot_zeros=False, thresh=thresh, show=False, maxDisp=maxDisp, includeTemp=includeTemp)
 					plt.savefig('images/{0}_diffab_{1}_{2}.png'.format(output, regions[i], regions[j]))
 	else:
 		for i in range(len(region_averages)):
 			for j in range(len(region_averages)):
 				if i < j:
-					L2_UniFrac, DifferentialAbundance = L2U.L2Unifrac_weighted(Tint, lint, nodes_in_order, region_averages[regions[i]], region_averages[regions[j]], include_tmp_diffab=False)
+					L2_UniFrac, DifferentialAbundance = L2U.L2Unifrac_weighted(Tint, lint, nodes_in_order, region_averages[regions[i]], region_averages[regions[j]], include_tmp_diffab=include_tmp_diffab)
 					fig = L2U.plot_diffab(nodes_in_order, taxonomy_in_order, DifferentialAbundance, regions[i], regions[j], plot_zeros=False, thresh=thresh, show=False, maxDisp=maxDisp, includeTemp=includeTemp)
 					plt.savefig('images/{0}_diffab_{1}_{2}.png'.format(output, regions[i], regions[j]))
